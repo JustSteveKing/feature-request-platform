f101ee5746d9d4a6967fedb47e680393cd48d5ba
Steve McDougall <juststevemcd@gmail.com>
2023-02-02 19:10:03 +0000

    Modelling user model

--- app/Enums/Role.php
+++ app/Enums/Role.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace App\Enums;
 
-enum Role
+enum Role: string
 {
-
+    case USER = 'user';
+    case ADMIN = 'admin';
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,34 +2,28 @@
 
 declare(strict_types=1);
 
+use App\Enums\Role;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table): void {
-            $table->id();
+        Schema::create('users', static function (Blueprint $table): void {
+            $table->ulid('id')->primary();
+
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default(Role::ADMIN->value);
             $table->rememberToken();
+
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
